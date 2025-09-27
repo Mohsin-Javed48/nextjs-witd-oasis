bb09707b30a90c074d85661dac62bf1d61d10c4f
Mohsin Javed <mohsinjavedpc@gmail.com>
2025-09-27 16:06:26 +0500

    auth section complete

--- app/_components/SignOutButton.tsx
+++ app/_components/SignOutButton.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/solid";
+import { signOut } from "../_lib/actions";
 
-function SignOutButton(): JSX.Element {
+function SignOutButton() {
   return (
-    <button
-      type="button"
-      className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full"
-    >
-      <ArrowRightStartOnRectangleIcon className="h-5 w-5 text-primary-600" />
-      <span>Sign out</span>
-    </button>
+    <form action={signOut}>
+      {" "}
+      <button
+        type="submit"
+        className="py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200 w-full"
+      >
+        <ArrowRightStartOnRectangleIcon className="h-5 w-5 text-primary-600" />
+        <span>Sign out</span>
+      </button>
+    </form>
   );
 }
 
